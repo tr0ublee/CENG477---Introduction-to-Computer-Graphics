bb1188de767d55d5c875b550abdfcac5fdc3aac2
tr0ublee <alperen.0311@gmail.com>
2020-11-12 20:27:50 +0300

    test runner added

---
+++ HW1/Code_Template/testRunner.py
@@ -0,0 +1,11 @@
+import os
+path = '../Sample_Scenes/Input_Files/'
+executable = './raytracer '
+os.system(executable + path + 'bunny.xml')
+os.system(executable + path + 'cornellbox.xml')
+os.system(executable + path + 'mirror_spheres.xml')
+os.system(executable + path + 'simple.xml')
+os.system(executable + path + 'simple_reflectance.xml')
+os.system(executable + path + 'simple_shading.xml')
+os.system(executable + path + 'horse_and_mug.xml')
+os.system(executable + path + 'killeroo.xml')
