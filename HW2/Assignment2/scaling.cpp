7b69cf78897949a20c1a7dbd3b067361ba98bc43
Aytac <aytacanildurmaz@gmail.com>
2020-12-09 13:47:51 +0300

    scaling abstraction

--- HW2/Assignment2/scaling.cpp
+++ HW2/Assignment2/scaling.cpp
@@ -7,8 +7,17 @@ namespace fst
         , sz(sz)
     {}
 
-    Matrix Scaling::getScalingMatrix () {
+    Matrix Scaling::getScalingMatrix() {
         Matrix scaleMatrix(sx, sy, sz, SCALE);
         return scaleMatrix;
+    }
+    Matrix Scaling::getScalingMatrixWRTFixedPoint(math::Vector3f& fixedPoint) {
+        Translation t(-fixedPoint.x, -fixedPoint.y, -fixedPoint.y);
+
+        Matrix S = this -> getScalingMatrix();
+        Matrix T1 = t.getTranslationMatrix();
+        Matrix T2 = t.getInverseTranslationMatrix();
+
+        return T2 * S * T1;
     }
 }
