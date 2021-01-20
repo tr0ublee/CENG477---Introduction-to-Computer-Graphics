6b62f2242005969475bb843076e5f5ae283a5d54
Aytac <aytacanildurmaz@gmail.com>
2021-01-20 21:49:35 +0300

    hw4 initial commit

---
+++ HW4/main.cpp
@@ -0,0 +1,93 @@
+#include "helper.h"
+#include "solution.h"
+
+static GLFWwindow* win = NULL;
+int widthWindow = 1000, heightWindow = 1000;
+
+// Shaders
+GLuint idProgramShader;
+GLuint idFragmentShader;
+GLuint idVertexShader;
+GLuint idJpegTexture;
+GLuint idHeightTexture;
+GLuint idMVPMatrix;
+
+// Buffers
+GLuint idVertexBuffer;
+GLuint idIndexBuffer;
+
+int textureWidth, textureHeight;
+float heightFactor;
+
+Camera camera;
+Camera initialCamera;
+
+glm::vec3 lightPos;
+GLuint depthMapFBO;
+GLuint depthCubemap;
+bool lightPosFlag = false;
+
+static void errorCallback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 3) {
+    printf("Please provide height and texture image files!\n");
+    exit(-1);
+  }
+
+  glfwSetErrorCallback(errorCallback);
+
+  if (!glfwInit()) {
+    exit(-1);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE); // This is required for remote
+  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE); // This might be used for local
+
+  win = glfwCreateWindow(widthWindow, heightWindow, "CENG477 - HW4", NULL, NULL);
+
+  if (!win) {
+      glfwTerminate();
+      exit(-1);
+  }
+  glfwMakeContextCurrent(win);
+
+  GLenum err = glewInit();
+  if (err != GLEW_OK) {
+      fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+
+      glfwTerminate();
+      exit(-1);
+  }
+
+  initShaders();
+  glUseProgram(idProgramShader);
+
+  glfwSetKeyCallback(win, keyCallback);
+
+  initTexture(argv[1], argv[2], &textureWidth, &textureHeight);
+  
+
+  while(!glfwWindowShouldClose(win)) {
+    glfwSwapBuffers(win);
+    glfwPollEvents();
+  }
+
+
+  glfwDestroyWindow(win);
+  glfwTerminate();
+  return 0;
+}
